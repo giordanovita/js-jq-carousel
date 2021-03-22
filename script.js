2989442472f5572629b41a1e2bc36603d05e298c
giordanovita <giordanovitag@libero.it>
2021-03-22 20:16:34 +0100

    carousel with indicators complete

--- script.js
+++ script.js
@@ -1,63 +1,88 @@
-
-
 function prev(){
-    var activeImg = $('.active');
+   var activeImg = $('.active');
 
-    if(activeImg.hasClass('first')){
+   if(activeImg.hasClass('first')){
 
-        prevImg = $('img.last')
-        activeImg.removeClass('active')
-        prevImg.addClass('active')
- 
-    
-     }else{
-        var prevImg = activeImg.prev('img');
+       prevImg = $('img.last')
+       activeImg.removeClass('active')
+       prevImg.addClass('active')
 
 
-        activeImg.removeClass('active')
-        prevImg.addClass('active')
-         }
+    }else{
+       var prevImg = activeImg.prev('img');
 
 
-
-
-
-
+       activeImg.removeClass('active')
+       prevImg.addClass('active')
+        }
 
 }
 
 
 function next(){
-    
-    var activeImg = $('.active');
-     if(activeImg.hasClass('last')){
 
-        nextImg = $('img.first')
-        activeImg.removeClass('active')
-        nextImg.addClass('active')
+   var activeImg = $('.active');
+   
+    if(activeImg.hasClass('last')){
+
+       nextImg = $('.first')
+       activeImg.removeClass('active')
+       nextImg.addClass('active')
+
+
+    }else{
+       var nextImg = activeImg.next('img');
+
+       activeImg.removeClass('active')
+       nextImg.addClass('active')
+    }
+ }
+
  
-    
-     }else{
-        var nextImg = activeImg.next('img');
+ function nextInd(){ 
+      
+     $(".next").click(function(){
+         var indicatore = $('.indicators ul .blue')
+         if(indicatore.hasClass('last')){
+          nextIndic=$(indicatore).next('li')
 
-        activeImg.removeClass('active')
-        nextImg.addClass('active')
-     }
+          nextIndic = $('li.first')
+          indicatore.removeClass('blue')
+          nextIndic.addClass('blue')
+         }else{
+            var nextIndic=$(indicatore).next('li')
+            indicatore.removeClass('blue')
+            nextIndic.addClass('blue')
+   
+         }
+})
+ }
 
+function prevInd(){ 
+      
+ 
+   $(".prev").click(function(){
+       var indicatore = $('.indicators ul .blue')
+       if(indicatore.hasClass('first')){
+        prevIndic=$(indicatore).prev('li')
 
-
+        prevIndic = $('li.last')
+        indicatore.removeClass('blue')
+        prevIndic.addClass('blue')
+       }else{
+          var prevIndic=$(indicatore).prev('li')
+          indicatore.removeClass('blue')
+          prevIndic.addClass('blue')
+ 
+       }
+})
 
 }
-
-
-
-
-
-
 
 function init(){
-   $('.prev').click(prev)
-   $('.next').click(next)
-
-}
-$(document).ready(init);
+  $('.prev').click(prev)
+  $('.next').click(next)
+  nextInd()
+  prevInd()
+ }
+$(document).ready(init); 
